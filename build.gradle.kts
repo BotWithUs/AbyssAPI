51a621bfaf49cbd7224dee3ca124dd4c6faf810a
David <david.schlachter@mindmatrix.net>
2022-07-26 00:05:03 -0700

    Added maven publishing to local.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.10"
     java
+    `maven-publish`
 }
 
 group = "org.example"
@@ -22,4 +23,15 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.abyss.api"
+            artifactId = "AbyssAPI"
+            version = "1.0-SNAPSHOT"
+            from(components["java"])
+        }
+    }
 }
